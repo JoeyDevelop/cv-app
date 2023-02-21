fcae3d07f67f7f2cde410f3937cfd215694d2846
Joey Patterson <joeypatterson177@gmail.com>
2023-02-21 12:53:47 -0500

    create cv-app files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Personal from './components/Personal';
+import './styles/Personal.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className='box'>
+        <Personal />
+        <div className='footer'>
+          <div className='buttons'>
+            <button>Edit</button>
+            <button> Submit</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Personal.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class Personal extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <form className='section'>
+          <div className='grid'>
+            <label>First name: </label>
+            <label>Last name: </label>
+            <label>Email: </label>
+            <label>Phone number: </label>
+          </div>
+          <div className='grid1'>
+            <input type="text"></input>
+            <input type="text"></input>
+            <input type="text"></input>
+            <input type="text"></input>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Personal
